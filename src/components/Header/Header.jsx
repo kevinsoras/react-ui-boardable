9d186b6c190b9ad3e0b5ce2b31a7c31da2a49049
kevinsoras <kevinalexandersoras@gmail.com>
2024-02-11 12:32:44 -0500

    feat: add correct redirect

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,30 @@
 import styles from "./styles.module.css"
 import svg from "../../assets/login-icon.svg"
+import { useNavigate } from 'react-router-dom';
+import { authProvider } from "../../auth";
+
 function Header() {
+  const navigate = useNavigate();
+
+  const handleHomePage =()=>{
+    navigate("/")
+  }
+  const handleLogout=()=>{
+    authProvider.logout()
+    navigate("/login")
+  }
   return (
     <>
       <header className={styles.container}>
       <div className={styles['container-items']}>
         <div className={styles['container-logo']}>
-        <img className={styles.logo} src={svg} /> 
-        <h1 className={styles.title}>Boardable</h1>
+        <img onClick={handleHomePage} className={styles.logo} src={svg} /> 
+        <h1 onClick={handleHomePage} className={styles.title}>Boardable</h1>
         </div>
         <div className={styles['container-buttons']}>
-          <button className={styles['button-account']}>My Account</button>
-          <button className={styles['button-logout']}>Logout</button>
+          <button onClick={()=>{navigate("/account")}} className={styles['button-account']}>My Account</button>
+          <button onClick={handleLogout} className={styles['button-logout']}>Logout</button>
         </div>
-
       </div>
       </header>
     </>
